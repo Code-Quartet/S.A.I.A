6db44cbd440ab0a10741a6489f37c7be6e64b9d4
Luis Edgardo Duno Castellano <luisdunocastellano@gmail.com>
2026-03-29 23:00:56 -0400

    upate app register student info course

--- app/section_main/Register_student.js
+++ app/section_main/Register_student.js
@@ -35,8 +35,8 @@ module.exports = function Register_student(parentWindow) {
 
     window_Register_student.loadFile('app/section_main/Register_Student.html');
 
-    // Herramientas de desarrollo
-//window_Register_student.webContents.openDevTools();
+    //Herramientas de desarrollo
+    //window_Register_student.webContents.openDevTools();
 
     // Bloquear nuevas ventanas (Forma moderna)
     window_Register_student.webContents.setWindowOpenHandler(() => {
